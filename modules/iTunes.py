638ba539cf2e52bffa56272d1d15029c803d056c
Hulkstern <rjbare3@gmail.com>
2023-09-20 00:21:03 -0400

    Created basic app/module files

---
+++ modules/iTunes.py
@@ -0,0 +1,5 @@
+import os
+
+#Raw data pulled from source should be stored in each python module until called for by main.py.
+#The only data that should exit this function should be in the standard format of
+#To-Do: specify standard data-format
